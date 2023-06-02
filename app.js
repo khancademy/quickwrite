75c25ab5de7f10fdb34eee48f71e68ba9ae70b55
Eminence <96856045+khancademy@users.noreply.github.com>
2023-06-01 20:01:34 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const { exec } = require('child_process');
+
 exec('npm install express', (error, stdout, stderr) => {
   if (error) {
     console.error(`Error installing Express: ${error.message}`);
@@ -8,22 +9,27 @@ exec('npm install express', (error, stdout, stderr) => {
     console.error(`npm error: ${stderr}`);
     return;
   }
-const express = require('express');
-const fs = require('fs');
-const app = express();
-const port = 3000;
-app.use(express.json());
-app.post('/store', (req, res) => {
-  const variables = req.body;
-  fs.appendFile('log.txt', JSON.stringify(variables) + '\n', (err) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Error storing variables');
-    } else {
-      res.send('Variables stored successfully');
-    }
+
+  const express = require('express');
+  const fs = require('fs');
+  const app = express();
+  const port = 3000;
+
+  app.use(express.json());
+
+  app.post('/store', (req, res) => {
+    const variables = req.body;
+    fs.appendFile('log.txt', JSON.stringify(variables) + '\n', (err) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error storing variables');
+      } else {
+        res.send('Variables stored successfully');
+      }
+    });
   });
-});
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
 });
